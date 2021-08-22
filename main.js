14171eb6c2d48af2be1ffe0678dc51b73f7cce70
EC2 Default User <ec2-user@ip-172-31-37-153.us-east-2.compute.internal>
2021-08-22 18:20:55 +0000

    電卓再提出

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-function get_calc(btn) {
-      if(btn.value == "=") {
-        document.dentaku.display.value = eval(document.dentaku.display.value);
-      } else if (btn.value == "C") {
-        document.dentaku.display.value = "";
-      } else {
-        document.dentaku.display.value += btn.value;
-        document.dentaku.multi_btn.value = "*";
-        document.dentaku.div_btn.value = "/";
-      }
-    }
+let display = document.getElementById("result");
+
+//打った数字と算術演算子を表示させる
+//012などとならないように注意
+function get_value(btn) {
+ if (display.value == "0") {
+  display.value = "" + btn.value;
+ } else {
+  display.value = display.value + btn.value;
+ }
+}
+
+//演算子の連続入力禁止
+function get_ope(btn) {
+ if (display.value.slice(-1) == "+") {
+  return display.value;
+ } else if (display.value.slice(-1) == "-") {
+  return display.value;
+ } else if (display.value.slice(-1) == "*") {
+  return display.value;
+ } else if (display.value.slice(-1) == "/") {
+  return display.value;
+ } 
+ display.value = display.value + btn.value;
+}
+
+/*********************************
+ イコールを押すと合計値を表示
+*********************************/
+function get_eq(btn) {
+ display.value = new Function("return " + display.value)();
+ if (btn.value != "="){
+  display.value = "";
+ }
+}
+
+/*********************************
+ クリアボタンを押下した時の処理
+*********************************/
+function get_cle(btn){
+display.value = "";
+}
